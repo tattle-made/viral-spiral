510acbdbfe6f08dfb23b2f8f797c65705a497de5
Denny <denny.george90@gmail.com>
2025-06-04 17:02:43 +0530

    fix: db variables in runtime

--- config/runtime.exs
+++ config/runtime.exs
@@ -21,20 +21,18 @@ if System.get_env("PHX_SERVER") do
 end
 
 if config_env() == :prod do
-  database_url =
-    System.get_env("DATABASE_URL") ||
-      raise """
-      environment variable DATABASE_URL is missing.
-      For example: ecto://USER:PASS@HOST/DATABASE
-      """
-
   maybe_ipv6 = if System.get_env("ECTO_IPV6") in ~w(true 1), do: [:inet6], else: []
 
   config :viral_spiral, ViralSpiral.Repo,
     # ssl: true,
-    url: database_url,
+    # url: database_url,
+    username: System.get_env("DATABASE_USER"),
+    password: System.get_env("DATABASE_PASSWORD"),
+    hostname: System.get_env("DATABASE_HOSTNAME"),
+    database: System.get_env("DATABASE_NAME"),
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-    socket_options: maybe_ipv6
+    socket_options: maybe_ipv6,
+    stacktrace: true
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
   # A default value is used in config/dev.exs and config/test.exs but you
