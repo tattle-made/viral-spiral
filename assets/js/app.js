830543d7485bd9f978114191e16d657aacd8ae0f
maanasb01 <maanasb01@gmail.com>
2025-07-15 16:56:23 +0530

    feat: add copy room link button

--- assets/js/app.js
+++ assets/js/app.js
@@ -73,7 +73,41 @@ window.addEventListener("audio:enable", async (event)=>{
   await window.player.setup()
 })
 
+window.addEventListener('load', function () {
+  const clipboard = FlowbiteInstances.getInstance('CopyClipboard', 'npm-install-copy-button');
+  const tooltip = FlowbiteInstances.getInstance('Tooltip', 'tooltip-copy-npm-install-copy-button');
 
+  const $defaultIcon = document.getElementById('default-icon');
+  const $successIcon = document.getElementById('success-icon');
+
+  const $defaultTooltipMessage = document.getElementById('default-tooltip-message');
+  const $successTooltipMessage = document.getElementById('success-tooltip-message');
+
+  clipboard.updateOnCopyCallback((clipboard) => {
+      showSuccess();
+
+      // reset to default state
+      setTimeout(() => {
+          resetToDefault();
+      }, 2000);
+  })
+
+  const showSuccess = () => {
+      $defaultIcon.classList.add('hidden');
+      $successIcon.classList.remove('hidden');
+      $defaultTooltipMessage.classList.add('hidden');
+      $successTooltipMessage.classList.remove('hidden');    
+      tooltip.show();
+  }
+
+  const resetToDefault = () => {
+      $defaultIcon.classList.remove('hidden');
+      $successIcon.classList.add('hidden');
+      $defaultTooltipMessage.classList.remove('hidden');
+      $successTooltipMessage.classList.add('hidden');
+      tooltip.hide();
+  }
+})
 
 // Define web components
 window.customElements.define("vs-calendar", CalendarElem);
--- lib/viral_spiral_web/live/multiplayer_waiting_room.ex
+++ lib/viral_spiral_web/live/multiplayer_waiting_room.ex
@@ -8,16 +8,60 @@ defmodule ViralSpiralWeb.MultiplayerWaitingRoom do
   def render(assigns) do
     ~H"""
     <div class="min-h-screen bg-[url('/images/bg-gray.jpg')] bg-cover bg-center bg-no-repeat px-4 flex items-center justify-center">
-      <div class="self-center text-center border border-4 border-[#3E6FF2] rounded-md p-8">
+      <div class="self-center text-center border-4 border-[#3E6FF2] rounded-md p-8">
         <p class="text-md mb-4 text-textcolor-light text-xl font-semibold flex items-center justify-center gap-2">
-          Share the
-          <a class="underline text-lg text-fuchsia-900" href={"/join/#{@room_name}"}>
-            Room Link
-          </a>
-          with other players
+          Share the Room Link with other players
+          <div class="w-full ">
+            <div class="relative">
+              <label for="npm-install-copy-button" class="sr-only">Label</label>
+              <input
+                id="npm-install-copy-button"
+                type="text"
+                class="col-span-6 bg-gray-50 border border-gray-300 text-gray-500 text-sm rounded-lg block w-full p-2.5 "
+                value={"#{@full_host}/join/#{@room_name}"}
+                disabled
+                readonly
+              />
+              <button
+                data-copy-to-clipboard-target="npm-install-copy-button"
+                data-tooltip-target="tooltip-copy-npm-install-copy-button"
+                class="absolute end-2 top-1/2 -translate-y-1/2  hover:bg-gray-100 rounded-lg p-2 inline-flex items-center justify-center"
+              >
+                <span id="default-icon">
+                  📋
+                </span>
+                <span id="success-icon" class="hidden">
+                  <svg
+                    class="w-3.5 h-3.5 text-blue-500"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 16 12"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M1 5.917 5.724 10.5 15 1.5"
+                    />
+                  </svg>
+                </span>
+              </button>
+              <div
+                id="tooltip-copy-npm-install-copy-button"
+                role="tooltip"
+                class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-xs opacity-0 tooltip "
+              >
+                <span id="default-tooltip-message">Copy to clipboard</span>
+                <span id="success-tooltip-message" class="hidden">Copied!</span>
+                <div class="tooltip-arrow" data-popper-arrow></div>
+              </div>
+            </div>
+          </div>
         </p>
 
-        <div class="mb-16">
+        <div class="mb-16 mt-4">
           <p :for={player <- @state.room.players} class="text-xl">
             <span class="text-[#C3268A] font-bold"><%= player %></span>
             <span class="text-textcolor-light"> has joined</span>
@@ -40,6 +84,17 @@ defmodule ViralSpiralWeb.MultiplayerWaitingRoom do
   end
 
   def handle_params(%{"room_name" => room_name}, uri, socket) do
+    %URI{host: host, scheme: scheme, port: port} = URI.parse(uri)
+
+    port_part =
+      case {scheme, port} do
+        {"https", 443} -> ""
+        {"http", 80} -> ""
+        _ -> ":#{port}"
+      end
+
+    full_host = "#{scheme}://#{host}#{port_part}"
+
     if connected?(socket) do
       PubSub.subscribe(ViralSpiral.PubSub, "waiting-room:#{room_name}")
     end
@@ -52,6 +107,7 @@ defmodule ViralSpiralWeb.MultiplayerWaitingRoom do
         |> assign(:room_name, room_name)
         |> assign(:room_gen, pid)
         |> assign(:state, ui_state)
+        |> assign(:full_host, full_host)
 
       {:noreply, socket}
     else
