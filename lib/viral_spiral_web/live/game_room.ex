3cfc44a45ed9605078dec34542d9876c5662448f
Aatman Vaidya <aatmanvaidya@gmail.com>
2025-07-22 14:53:43 +0530

    chore: end game screen in designer mode (#55)

--- lib/viral_spiral_web/live/game_room.ex
+++ lib/viral_spiral_web/live/game_room.ex
@@ -48,7 +48,7 @@ defmodule ViralSpiralWeb.GameRoom do
     action = Actions.pass_card(Actions.string_to_map(params))
     gen_state = GenServer.call(room_gen, action)
     room_state = StateAdapter.game_room(gen_state)
-    socket = assign(socket, :state, room_state)
+    socket = socket |> assign(:state, room_state) |> maybe_put_end_banner(room_state)
     {:noreply, socket}
   end
 
