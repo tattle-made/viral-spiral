59bd3e78e07d1fee31490871abc459383817a0d0
Aatman Vaidya <aatmanvaidya@gmail.com>
2025-09-02 11:37:22 +0530

    chore: comment multiplayer broadcast

--- lib/viral_spiral_web/live/multiplayer_room.ex
+++ lib/viral_spiral_web/live/multiplayer_room.ex
@@ -235,13 +235,13 @@ defmodule ViralSpiralWeb.MultiplayerRoom do
   end
 
   def handle_info({:change_reasons, message_list}, socket) do
-    %{room_name: room_name} = socket.assigns
+    # %{room_name: room_name} = socket.assigns
 
-    PubSub.broadcast(
-      ViralSpiral.PubSub,
-      "multiplayer-room:#{room_name}",
-      {:change_reasons, message_list}
-    )
+    # PubSub.broadcast(
+    #   ViralSpiral.PubSub,
+    #   "multiplayer-room:#{room_name}",
+    #   {:change_reasons, message_list}
+    # )
 
     socket = socket |> assign(:change_list, message_list)
     {:noreply, socket}
