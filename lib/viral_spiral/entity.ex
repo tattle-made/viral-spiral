51f88997c55b190bc696ba12c6bc3870380614d1
dennyabrain <denny.george90@gmail.com>
2025-09-02 15:06:21 +0530

    fix: player is able to win now

--- lib/viral_spiral/entity.ex
+++ lib/viral_spiral/entity.ex
@@ -4,15 +4,9 @@ defmodule ViralSpiral.Entity do
   """
   alias ViralSpiral.Entity.Room.Changes.EndGame
 
-  def make_game_end_change({:no_over}) do
-    %EndGame{}
-  end
+  def make_game_end_change({:no_over}), do: %EndGame{}
 
-  def make_game_end_change({:over, :world}) do
-    %EndGame{reason: :world}
-  end
+  def make_game_end_change({:over, :world}), do: %EndGame{reason: :world}
 
-  def make_game_end_change({:over, :player, player_id}) when is_bitstring(player_id) do
-    %EndGame{reason: :player, winner_id: player_id}
-  end
+  def make_game_end_change({:over, :player}), do: %EndGame{reason: :player}
 end
