4d614b480842d7d68286c096941f570ba576729e
maanasb01 <maanasb01@gmail.com>
2025-09-03 10:18:50 +0530

    fix: disappearance of voting modal due to flash auto-hide

--- assets/js/flash.js
+++ assets/js/flash.js
@@ -1,19 +1,26 @@
 export const FlashAutoHide = {
     mounted() {
+        console.log("Flash auto hide hook mounted", this.el);
         const msAttr = this.el.getAttribute("data-timeout-ms");
-        const timeout = parseInt(msAttr || "0", 10);
+        const timeout = parseInt(msAttr || "4000", 10);
         if (!Number.isFinite(timeout) || timeout <= 0) return;
 
-        this.timer = setTimeout(() => {
-            // Trigger the same behavior as manual close by clicking the element
-            // which has a phx-click to clear and hide itself
-            this.el.click();
+        this.el.style.transition = 'opacity 0.5s ease-out';
+
+        this.hideTimer = setTimeout(() => {
+
+            this.el.style.opacity = '0';
+
+            this.removeTimer = setTimeout(() => {
+                if (this.el && this.el.parentNode) {
+                    this.el.parentNode.removeChild(this.el);
+                }
+            }, 500);
         }, timeout);
     },
+
     destroyed() {
-        if (this.timer) {
-            clearTimeout(this.timer);
-            this.timer = null;
-        }
+        if (this.hideTimer) clearTimeout(this.hideTimer);
+        if (this.removeTimer) clearTimeout(this.removeTimer);
     }
 };
