48be36e7c331bc85d959372ecad0f3e944e18a21
dennyabrain <denny.george90@gmail.com>
2026-09-13 11:54:02 +0530

    fix: whitelist itch.io as origin

--- config/prod.exs
+++ config/prod.exs
@@ -6,7 +6,8 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :viral_spiral, ViralSpiralWeb.Endpoint,
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  check_origin: ["https://viralspiral.tattle.co.in", "https://*.itch.io"]
 
 # Configures Swoosh API Client
 config :swoosh, api_client: Swoosh.ApiClient.Finch, finch_name: ViralSpiral.Finch
